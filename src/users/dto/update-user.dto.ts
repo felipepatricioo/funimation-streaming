f932aa62e08246bca698aed6ee132f8a0ffdd7bb
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2021-12-19 15:06:57 -0300

    adding cors to main.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,5 +3,11 @@ import { IsString, IsEmail, IsOptional, IsNotEmpty } from 'class-validator';
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
